e44ac1c909ba7a3f006c2c5f003a99e976752504
gmaiferreira <goncalo.ferreira@parceiros.nos.pt>
2025-05-16 20:35:31 +0100

    Adicionada pasta Challenges da comunidade Viver de Codigo

---
+++ Challenges/Pag-Login-Viver-Codigo/assets/js/main.js
@@ -0,0 +1,18 @@
+const showPasswordBtn = document.getElementById("showPasswordBtn");
+
+showPasswordBtn.addEventListener("click",toggleVisibility);
+
+function toggleVisibility() {
+    const passwordInput = document.getElementById("password");
+    if (passwordInput.type == "password"){
+        passwordInput.type = "text";
+        showPasswordBtn.innerText = "visibility_off";
+    } else {
+        passwordInput.type = "password";
+        showPasswordBtn.innerText = "visibility";
+    }
+
+}
+
+
+
